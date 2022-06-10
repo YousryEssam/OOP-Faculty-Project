ced9f090a16bd377ae6c0220849b37b5e7e25271
Yousry Essam <77233074+YousryEssam@users.noreply.github.com>
2022-06-10 15:56:05 +0200

    Update FCI.java

--- Faculty/src/main/java/com/mycompany/faculty/FCI.java
+++ Faculty/src/main/java/com/mycompany/faculty/FCI.java
@@ -1,20 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 package com.mycompany.faculty;
 
-/**
- *
- * @author Morcos Shehata
- */
 public interface FCI 
 {
    String collage_name="Computers and Information";
     String university="Assiut";
-    
-   
-
-    
-    
 }
