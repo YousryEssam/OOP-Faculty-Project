37efce24fb4b75619ffaab1fc022199ee9701de7
Yousry Essam <77233074+YousryEssam@users.noreply.github.com>
2022-06-10 16:00:11 +0200

    Update Workers.java

--- Faculty/src/main/java/com/mycompany/faculty/Workers.java
+++ Faculty/src/main/java/com/mycompany/faculty/Workers.java
@@ -1,13 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.faculty;
-
-/**
- *
- * @author Morcos Shehata
- */
 public class Workers extends Staff
 {
 
@@ -25,7 +16,4 @@ public class Workers extends Staff
     public String toString() {
         return "Worker{"+ "name=" + name + ", age=" + age + ", nationality=" + nationality + ", address=" + address + ", gov_id=" + gov_id  +", gender=" + gender + "salary=" + salary + ", department=" + department + ", starting_year=" + starting_year + ", added succefully!"+'}';
     }
-
-
-    
 }
