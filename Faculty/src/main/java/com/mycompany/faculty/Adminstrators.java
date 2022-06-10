50971e723225c77909fe59c955d695e8b51ce0d5
Yousry Essam <77233074+YousryEssam@users.noreply.github.com>
2022-06-10 15:55:10 +0200

    Update Adminstrators.java

--- Faculty/src/main/java/com/mycompany/faculty/Adminstrators.java
+++ Faculty/src/main/java/com/mycompany/faculty/Adminstrators.java
@@ -1,13 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.faculty;
-
-/**
- *
- * @author Morcos Shehata
- */
 public class Adminstrators extends Staff
 {   
     private String Position;
@@ -19,10 +10,6 @@ public class Adminstrators extends Staff
         super(salary, department, starting_year, name, age, nationality, gender, address, gov_id);
         this.Position = Position;
     }
-    
-
-    
-
     public String getPosition() {
         return Position;
     }
